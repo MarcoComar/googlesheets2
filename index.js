b2b6401f8ceabb79e82bf4c1965c6189437cc5aa
MarcoComar <34415721+MarcoComar@users.noreply.github.com>
2021-03-11 18:25:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function testGS() {
     
 }
 
+function addGS() {
+    var url = "https://script.google.com/a/yourdomain.com/macros/s/AKfycbxf8eYSOxDxza_vyEMlROLybIKxxHS1gvdXm3-p0Q/exec";
+
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify({name: "comar"})
+    });
+    
+    
+}
+
+document.getElementById("btn2").addEventListener("click", addGS);
+
 document.getElementById("btn").addEventListener("click", testGS);
